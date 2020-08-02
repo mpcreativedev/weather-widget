4f39b75c6f62ec44d86ebabcf8da8502d5bf29b8
Mauro Piccin <mauro.pccn@gmail.com>
2020-08-02 17:11:51 +0200

    date fix update

--- src/filters/index.ts
+++ src/filters/index.ts
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 
 Vue.filter('timeDate', (value: string): string => {
-  const date = new Date(value)
+  const date = new Date(value.replace(' ', 'T'))
   return date.toLocaleString()
 })
 
-Vue.filter('date', (value: string): string => {
-  const date = new Date(value)
+Vue.filter('date', (value: string): any => {
+  const date = new Date(value.replace('+0200', ''))
   const day = ('0' + date.getDate()).slice(-2)
   const month = ('0' + (date.getMonth() + 1)).slice(-2)
   return `${day}/${month}`
